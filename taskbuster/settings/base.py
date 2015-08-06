73d475083866c1eaae233801c6bb04b3efe57051
branebrvl <branebrvl@gmail.com>
2015-08-05 22:54:08 -0700

    configure db

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -86,10 +86,20 @@ WSGI_APPLICATION = 'taskbuster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
